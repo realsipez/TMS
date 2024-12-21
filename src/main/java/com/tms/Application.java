7798f6b4a0219ca6e68e4beb4e73eaed53818b78
sina <realsipez@gmail.com>
2024-12-21 16:35:45 +0330

    adding some functionality and fix some bugs

--- src/main/java/com/tms/Application.java
+++ src/main/java/com/tms/Application.java
@@ -34,16 +34,18 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User admin = new User();
-        admin.setFullName("Admin");
-        admin.setUsername("admin");
-        admin.setPassword(passwordEncoder.encode("Sp@123456"));
-        Role adminRole = new Role();
-        adminRole.setName("ADMIN");
-        adminRole.setDescription("Administrator");
-        roleRepository.save(adminRole);
-        admin.setRole(Sets.newHashSet(adminRole));
-        admin.setCreatedAt(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
-        userRepository.save(admin);
+        if (!userRepository.existsUserByUsername("admin") && !roleRepository.existsByName("ADMIN")) {
+            User admin = new User();
+            admin.setFullName("Admin");
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("Sp@123456"));
+            Role adminRole = new Role();
+            adminRole.setName("ADMIN");
+            adminRole.setDescription("Administrator");
+            roleRepository.save(adminRole);
+            admin.setRole(Sets.newHashSet(adminRole));
+            admin.setCreatedAt(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+            userRepository.save(admin);
+        }
     }
 }
--- src/main/java/com/tms/domain/repository/user/UserRepository.java
+++ src/main/java/com/tms/domain/repository/user/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    boolean existsUserByUsername(String username);
 }
